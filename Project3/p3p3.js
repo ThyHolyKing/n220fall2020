da48e916dcba6a847ba7efb4a348d794f2bc8657
ThyHolyKing <70738422+ThyHolyKing@users.noreply.github.com>
2020-09-24 23:35:22 -0400

    Create p3p3.js

---
+++ Project3/p3p3.js
@@ -0,0 +1,16 @@
+function setup() {
+    createCanvas(600, 600);
+    background(200);
+}
+
+function draw() {
+    background(200);
+
+
+for (let i = 0; i < 6; ++ i){
+    if i = 0 {
+        square(10, 10, 100);
+    }
+    else if i = 1 {square(10, 30, 100);}
+}
+}
