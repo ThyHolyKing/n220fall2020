c6d1ccd8cc975f570564533567499afdf435c037
ThyHolyKing <70738422+ThyHolyKing@users.noreply.github.com>
2020-09-24 23:35:20 -0400

    Create p3p2.js

---
+++ Project3/p3p2.js
@@ -0,0 +1,13 @@
+function setup() {
+    createCanvas(600, 600);
+    background(200);
+}
+
+function draw() {
+    background(200);
+
+
+for (let i = 0; i < 43; ++ i){
+    circle(300, 300, 300-i*7);
+}
+}
