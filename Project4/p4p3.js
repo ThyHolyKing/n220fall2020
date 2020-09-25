288d25a5008559e9373d594c4963a7bf599b92f5
ThyHolyKing <70738422+ThyHolyKing@users.noreply.github.com>
2020-09-24 23:35:38 -0400

    Create p4p3.js

---
+++ Project4/p4p3.js
@@ -0,0 +1,28 @@
+var circle_coords=[];
+var frames_count=0; 
+var diameter=20; 
+function setup() {
+createCanvas(400, 400);
+}
+function draw() {
+
+background(80);
+
+for(let i = 0; i < circle_coords.length; i++){
+    ellipse(width/2,circle_coords[i],diameter,diameter);
+    circle_coords[i]=circle_coords[i]+5;
+}
+frames_count=frames_count+1;
+
+if(frames_count==10){
+    frames_count=0;
+    circle_coords.push(0);
+    if(circle_coords.length>100){
+
+      circle_coords.shift();
+
+    }
+
+}
+
+}
